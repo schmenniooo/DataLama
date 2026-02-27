9314883bac834cf57af8cd43831411ae7a218e5b
ennio <ennio.nick@gmail.com>
2026-02-27 10:43:25 +0100

    using uvicorn run directly

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 """Entry point for the DataLama application."""
 
 from src.server.server import Server
+import uvicorn
+import os
 
 def main():
     app = Server().use_authenticaton().build().run()
-    return app
+    uvicorn.run(
+        app, 
+        host=os.getenv("HOST", "127.0.0.1"), 
+        port=os.getenv("PORT", 3000)
+    )
 
 if __name__ == "__main__":
     print("Hello from datalama!")
