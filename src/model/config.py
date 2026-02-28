7be7e36fcd3bba2d16e4179b591bd120c72fb4d4
ennio <ennio.nick@gmail.com>
2026-02-28 18:47:28 +0100

    config class added

---
+++ src/model/config.py
@@ -0,0 +1,10 @@
+# Stores global microservice configuration
+
+class Config: 
+    api_key_field_name: str
+    api_key: str
+    ollama_base_url: str
+    ollama_model: str
+    debug: bool
+    host: str
+    port: int
