c33b7eb208d3e0ff63010a1a4d368e3c36f31bf1
ennio <ennio.nick@gmail.com>
2026-02-28 19:05:31 +0100

    config as data class

--- src/model/config.py
+++ src/model/config.py
@@ -1,5 +1,8 @@
 # Stores global microservice configuration
 
+from dataclasses import dataclass                                       
+                  
+@dataclass 
 class Config: 
     api_key_field_name: str
     api_key: str
