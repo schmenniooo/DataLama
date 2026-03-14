86bba398177c24859fdb350b0fee347b0c8d5018
ennio <ennio.nick@gmail.com>
2026-03-14 19:12:42 +0100

    data sets validator function impl

--- src/validation/validation.py
+++ src/validation/validation.py
@@ -1,3 +1,12 @@
+"""Client input validation module"""
 
-def validate_data_sets(data_sets: list[str]):
-    pass
+def validate_data_sets(data_sets: list[str]) -> bool:
+    if len(data_sets) == 0:
+        return False
+
+    for data_set in data_sets:
+        if len(data_set) == 0:
+            return False
+
+    return True
+
