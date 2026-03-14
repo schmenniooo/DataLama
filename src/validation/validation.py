0c0fdd52e5f12f800ccb940c8ba894e00ce51f5e
ennio <ennio.nick@gmail.com>
2026-03-14 19:18:06 +0100

    returning message instead of bool

--- src/validation/validation.py
+++ src/validation/validation.py
@@ -1,12 +1,12 @@
 """Client input validation module"""
 
-def validate_data_sets(data_sets: list[str]) -> bool:
+def validate_data_to_analyse(data_sets: list[str]) -> str:
     if len(data_sets) == 0:
-        return False
+        return "Data cannot be empty"
 
     for data_set in data_sets:
         if len(data_set) == 0:
-            return False
+            return "Single data sets cannot be emtpy"
 
-    return True
+    return ""
 
