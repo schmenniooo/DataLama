2e7bb6d4cf7dca69cdc5a086f75fddb5a828e285
ennio <ennio.nick@gmail.com>
2026-03-22 11:02:47 +0100

    creating client

--- src/ai/langsmith/langsmith_handler.py
+++ src/ai/langsmith/langsmith_handler.py
@@ -1,9 +1,11 @@
 """Module to handle tracing with LangSmith"""
 
+from langsmith import Client
+
 class LangSmithHandler:
 
     def __init__(self, api_key: str, project_name: str):
-        pass
+        self.client = Client(api_key=api_key)
 
     def check_langsmith_connection(self) -> bool:
         pass
