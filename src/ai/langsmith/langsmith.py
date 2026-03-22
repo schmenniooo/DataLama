6ae4ee0e9831035253ca0f8be789cab76beeb06a
ennio <ennio.nick@gmail.com>
2026-03-22 10:40:11 +0100

    base structure for langsmith handler

--- src/ai/langsmith/langsmith.py
+++ src/ai/langsmith/langsmith.py
@@ -1,1 +1,9 @@
 """Module to handle tracing with LangSmith"""
+
+class LangSmithHandler:
+
+    def __init__(self, api_key: str, project_name: str):
+        pass
+
+    def check_langsmith_connection(self) -> bool:
+        pass
