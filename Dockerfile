fcc320d0e8b6c094aea2f5c85864a0102ec9cb84
ennio <ennio.nick@gmail.com>
2026-04-06 10:19:18 +0200

    config file in env

--- Dockerfile
+++ Dockerfile
@@ -19,22 +19,24 @@ FROM python:3.13-slim AS runtime
 COPY --from=ghcr.io/astral-sh/uv:0.8.21 /uv /uvx /bin/
 WORKDIR /app
 
-# Using build stage
+# Using build stage for dependencies
 COPY --from=build /app .
-
-# Setting environment variables
 
 # LLM Communication
 ENV LLM_PROVIDER="anthropic"
 ENV LLM_PROVIDER_API_TOKEN="anthropic-api-key"
 ENV MODEL="claude-sonnet-4-6"
 
+# Knowledge Base Handling
+ENV KNOWLEDGEBASE_ENABLED="false"
+ENV KNOWLEDGEBASE_CONFIG_PATH="/app/config/knowledge_bases.yml"
+
 # Langsmith Integration
 ENV LANGSMITH_TRACING="false"
 ENV LANGSMITH_API_KEY="langsmith-api-key"
 ENV LANGSMITH_PROJECT="my-project"
 
-# Application Config
+# Service Config
 ENV DEBUG="false"
 ENV HOST="0.0.0.0"
 ENV PORT=3000
--- src/main.py
+++ src/main.py
@@ -18,15 +18,17 @@ def main():
     config = Config(
         api_key_field_name = os.getenv("API_KEY_FIELD_NAME", ""),
         api_key = os.getenv("API_KEY", ""),
-        debug = os.getenv("DEBUG", "false").lower() == "true",
-        host = os.getenv("HOST", "0.0.0.0"),
-        port = int(os.getenv("PORT", "3000")),
         llm_provider=os.getenv("LLM_PROVIDER", ""),
         llm_provider_api_token = os.getenv("LLM_PROVIDER_API_TOKEN", ""),
         model = os.getenv("MODEL", ""),
+        knowledge_base_enabled=os.getenv("KNOWLEDGE_BASE_ENABLED", "false").lower() == "true",
+        knowledge_base_config_path=os.getenv("KNOWLEDGE_BASE_CONFIG_PATH", ""),
         langsmith_tracing_enabled=os.getenv("LANGSMITH_TRACING_ENABLED", "false").lower() == "true",
         langsmith_api_key = os.getenv("LANGSMITH_API_KEY", ""),
         langsmith_project = os.getenv("LANGSMITH_PROJECT", ""),
+        debug=os.getenv("DEBUG", "false").lower() == "true",
+        host=os.getenv("HOST", "0.0.0.0"),
+        port=int(os.getenv("PORT", "3000")),
     )
 
     # Creating and running server
