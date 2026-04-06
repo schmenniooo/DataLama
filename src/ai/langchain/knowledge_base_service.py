65bbdfde0daac9f8ef8861bd32f85bbdf93d28f0
ennio <ennio.nick@gmail.com>
2026-04-06 10:09:08 +0200

    base method structure

--- src/ai/langchain/knowledge_base_service.py
+++ src/ai/langchain/knowledge_base_service.py
@@ -1,1 +1,15 @@
 """Module to fetch data from configured knowledge base"""
+
+class KnowledgeBaseService:
+
+    def __init__(self, providers: list):
+        pass
+
+    def knowledge_base_fetch_workflow(self):
+        pass
+
+    def get_knowledge_base_data(self):
+        pass
+
+    def push_knowledge_base_data_to_vector_store(self):
+        pass
